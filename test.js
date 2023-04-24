ff14144646cd7f08a2905dce3f7536b90ece5f0c
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-04-24 10:17:52 +0200

    J'ai fini la mise en forme et j'ai rempli le tableau de citations

--- test.js
+++ test.js
@@ -1,15 +1,44 @@
+let button = document.querySelector('button');
+let div = document.querySelector('div');
+
+let citation1 = "L’imagination gouverne le monde.";
+let auteur1   = "Napoléon";
+
+let citation2 = "Agissez comme s'il était impossible d'échouer.";
+let auteur2   = "Winston Churchill";
+
+let citation3 = "Celui qui accepte le mal sans lutter contre lui coopère avec lui.";
+let auteur3   = "Martin Luther King";
+
+let citation4 = "À vaincre sans péril, on triomphe sans gloire.";
+let auteur4   = "Pierre Corneille";
+
+let citation5 = "Fais de ta vie un rêve, et d'un rêve, une réalité.";
+let auteur5   = "Antoine de Saint-Exupéry";
+
+let citation6 = "Un gagnant est un rêveur qui n'abandonne jamais.";
+let auteur6   = "Nelson Mandela";
+
+let citation7 = "Celui qui ne connaît pas l'histoire est condamné à la revivre.";
+let auteur7   = "Karl Marx";
+
+let citation8 = "Qui veut la paix prépare la guerre.";
+let auteur8   = "Jules César";
+
+
+
 let tableauDeCitation = [
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  ""
-]
+  [citation1, auteur1],
+  [citation2, auteur2],
+  [citation3, auteur3],
+  [citation4, auteur4],
+  [citation5, auteur5],
+  [citation6, auteur6],
+  [citation7, auteur7],
+  [citation8, auteur8]
+];
+
+
 
 
 
