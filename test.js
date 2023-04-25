b59dcdd468db6aeef88a8239806210b6b088bf08
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-04-25 15:23:39 +0200

    J'ai apporté quelques modifications au texte de la citation, j'y ai mis des guillemets et j'ai appliqué un style italic au texte

--- test.js
+++ test.js
@@ -4,28 +4,28 @@ let bouton = document.querySelector("button");
 let i = 0;
 
 // Crétaions des variables qui contiennent chacune une citation
-let citation1 = "L’imagination gouverne le monde.";
+let citation1 = "\"L’imagination gouverne le monde.\"";
 let auteur1   = "Napoléon";
 
-let citation2 = "Agissez comme s'il était impossible d'échouer.";
+let citation2 = "\"Agissez comme s'il était impossible d'échouer.\"";
 let auteur2   = "Winston Churchill";
 
-let citation3 = "Celui qui accepte le mal sans lutter contre lui coopère avec lui.";
+let citation3 = "\"Celui qui accepte le mal sans lutter contre lui coopère avec lui.\"";
 let auteur3   = "Martin Luther King";
 
-let citation4 = "À vaincre sans péril, on triomphe sans gloire.";
+let citation4 = "\"À vaincre sans péril, on triomphe sans gloire.\"";
 let auteur4   = "Pierre Corneille";
 
-let citation5 = "Fais de ta vie un rêve, et d'un rêve, une réalité.";
+let citation5 = "\"Fais de ta vie un rêve, et d'un rêve, une réalité.\"";
 let auteur5   = "Antoine de Saint-Exupéry";
 
-let citation6 = "Un gagnant est un rêveur qui n'abandonne jamais.";
+let citation6 = "\"Un gagnant est un rêveur qui n'abandonne jamais.\"";
 let auteur6   = "Nelson Mandela";
 
-let citation7 = "Celui qui ne connaît pas l'histoire est condamné à la revivre.";
+let citation7 = "\"Celui qui ne connaît pas l'histoire est condamné à la revivre.\"";
 let auteur7   = "Karl Marx";
 
-let citation8 = "Qui veut la paix prépare la guerre.";
+let citation8 = "\"Qui veut la paix prépare la guerre.\"";
 let auteur8   = "Jules César";
 
 
@@ -55,6 +55,7 @@ function nouvelleCitation(){
     document.getElementById("auteur").textContent = tableauDeCitation[i][1];
     i++;
 
+    // Condition qui permet de repasser l'index à 0 quand celui-ci arrive à la fin du tableau
     if(i >= tableauDeCitation.length){
       i = 0;
     }
